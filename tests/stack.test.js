50bdcde65173571cd093c61b4f0208610b521fec
crypto-Mil <emilfors@icloud.com>
2026-05-12 21:45:52 +0200

    commit med fel

---
+++ tests/stack.test.js
@@ -0,0 +1,29 @@
+const stack = require('../src/stack');
+
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
+
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
+
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});
+
+test('pop removes the top element from the stack', () => {
+    stack.push(10);
+    stack.push(20);
+
+    const removed = stack.pop();
+
+    expect(removed).toBe(10);
+    expect(stack.peek()).toBe(10);
+});
